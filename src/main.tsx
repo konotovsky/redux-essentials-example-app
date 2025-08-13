99b5420fbf718a55b59cbbf7b465703c71d3dd93
Vladyslav Konotovskyi <konotovsky@gmail.com>
2025-08-13 11:31:40 +0000

    Add PostsList

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 
 import App from './App'
+import { store } from './app/store'
 
 import { worker } from './api/server'
 
-import './primitiveui.css'
 import './index.css'
+import './primitiveui.css'
 
 // Wrap app rendering so we can wait for the mock API to initialize
 async function start() {
@@ -17,7 +19,9 @@ async function start() {
 
   root.render(
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>,
   )
 }
