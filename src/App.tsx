44bc1c39995757546e3d76a1602ef958800d7f2c
Vladyslav Konotovskyi <konotovsky@gmail.com>
2025-08-14 09:59:33 +0000

    Implement editing post

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
 import { Navbar } from './components/Navbar'
+import { EditPostForm } from './features/posts/EditPostForm'
 import { PostsMainPage } from './features/posts/PostsMainPage'
 import { SinglePostPage } from './features/posts/SinglePostPage'
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<PostsMainPage />}></Route>
           <Route path="/posts/:postId" element={<SinglePostPage />}></Route>
+          <Route path="/editPost/:postId" element={<EditPostForm />} />
         </Routes>
       </div>
     </Router>
