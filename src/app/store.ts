e2bdc1156b74cef6eef126184a8c7a5990b5a676
Lenovo <konotovsky@gmail.com>
2025-08-14 15:30:03 +0300

    Adding a Users Slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import postsReducer from '@/features/posts/postsSlice'
+import usersReducer from '@/features/users/usersSlice'
 import { configureStore } from '@reduxjs/toolkit'
 
 export const store = configureStore({
   reducer: {
     posts: postsReducer,
+    users: usersReducer,
   },
 })
 
